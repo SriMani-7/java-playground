2baa0c85ac22eb824459bfb0bfb6cac202ba9eac
SriMani-7 <basinenisrimanikanta@gmail.com>
2023-05-08 10:56:49 +0530

    completed

---
+++ tic-tac-toe/src/tictactoe/BoardLogic.java
@@ -0,0 +1,69 @@
+package tictactoe;
+
+import java.util.Arrays;
+
+public class BoardLogic {
+    private boolean isXTurn = true;
+    private final int[][] board;
+    private boolean isCompleted = false;
+
+    public BoardLogic() {
+        board = new int[3][3];
+    }
+
+    public void reset() {
+        for (int[] row : board) {
+            Arrays.fill(row, 0);
+        }
+        isXTurn = true;
+        isCompleted = false;
+    }
+
+    public State makeMove(int x, int y) {
+        if ((x >= 0 && x < 3) && (y >= 0 && y < 3)) {
+            if (!isCompleted && board[x][y] == 0) {
+                isXTurn = !isXTurn;
+                board[x][y] = isXTurn ? 1 : 2;
+                isCompleted = checkWinner();
+                return isCompleted ? State.WINNER : (isGameTie() ? State.TIE : State.MOVE);
+            }
+        }
+        return State.INVALID;
+    }
+
+    public String currentPlayer() {
+        return isXTurn ? "X" : "O";
+    }
+
+    public String opponent() {
+        return isXTurn ? "O" : "X";
+    }
+
+    public boolean checkWinner() {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != 0 && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+            if (board[0][i] != 0 && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return true;
+            }
+        }
+        // Check diagonals for a win
+        if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        }
+        return board[0][2] != 0 && board[0][2] == board[1][1] && board[1][1] == board[2][0];
+    }
+
+    public boolean isGameTie() {
+        for (int[] row : board) {
+            for (int cell : row) {
+                if (cell == 0) return false;
+            }
+        }
+        isCompleted = true;
+        return true;
+    }
+
+    enum State {MOVE, WINNER, TIE, INVALID}
+}
--- tic-tac-toe/src/tictactoe/GamePanel.java
+++ tic-tac-toe/src/tictactoe/GamePanel.java
@@ -1,24 +1,33 @@
 package tictactoe;
 
 import javax.swing.*;
-import javax.swing.border.BevelBorder;
 import java.awt.*;
 
 public class GamePanel extends JPanel {
 
-    public static final int BUTTON_SIZE = 90, BUTTON_GAP = 3;
-    public static final Color
-            BOXCOLOR = new Color(0xfa9884),
-            BUTTONCOLOR = new Color(0xfff3e2);
+    private final BoardLogic boardLogic;
+    private final JButton[][] buttons;
+    private final JLabel messageLabel;
 
     public GamePanel() {
+        boardLogic = new BoardLogic();
+        buttons = new JButton[3][3];
         var box = new BoxLayout(this, BoxLayout.Y_AXIS);
         setLayout(box);
-        setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        setBackground(BOXCOLOR);
+        setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         // Title label
         JLabel titleLabel = new JLabel("Tic Tac Toe");
-        titleLabel.setFont(new Font("Verdana", Font.BOLD, 22));
+        titleLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+
+        titleLabel.setForeground(new Color(0xDDE8E9));
+
+        messageLabel = new JLabel("X");
+        messageLabel.setFont(new Font("verdana", Font.BOLD, 20));
+        messageLabel.setForeground(new Color(0x3FE4C0));
+
+        JPanel gridPanel = new JPanel();
 
         // restart button
         JButton restartButton = new JButton("Restart");
@@ -26,27 +35,28 @@ public class GamePanel extends JPanel {
         restartButton.setContentAreaFilled(false);
         restartButton.setFont(restartButton.getFont().deriveFont(Font.BOLD, 16f));
         restartButton.setBorderPainted(false);
+        restartButton.setForeground(new Color(0xF6D220));
+        restartButton.addActionListener(event -> resetGame());
 
         // message label
-        JLabel messageLabel = new JLabel("X move");
-        messageLabel.setFont(new Font("Comic sans MS", Font.BOLD, 20));
 
-        JPanel gridPanel = new JPanel();
         gridPanel.setMaximumSize(grisSize());
         gridPanel.setLayout(new GridLayout(3, 3, BUTTON_GAP, BUTTON_GAP));
-        gridPanel.setBackground(BOXCOLOR);
-        gridPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        gridPanel.setBackground(BUTTONCOLOR);
         for (int x = 0; x < 3; x++) {
             for (int y = 0; y < 3; y++) {
-                JButton button = new JButton("");
+                int X = x, Y = y;
+                JButton button = new JButton(" ");
                 button.setSize(BUTTON_SIZE, BUTTON_SIZE);
-                button.setBackground(BUTTONCOLOR);
                 button.setFocusPainted(false);
+                button.setContentAreaFilled(false);
                 button.setFont(new Font("cursive", Font.PLAIN, 34));
                 button.setBorderPainted(false);
-                button.addActionListener(event -> {
-
-                });
+                button.setOpaque(true);
+                button.setBackground(BOXCOLOR);
+                button.setForeground(new Color(0xFDFDFD));
+                button.addActionListener(event -> onButtonClick(X, Y));
+                buttons[x][y] = button;
                 gridPanel.add(button);
             }
         }
@@ -66,9 +76,38 @@ public class GamePanel extends JPanel {
 
     }
 
+    private void resetGame() {
+        boardLogic.reset();
+        for (JButton[] row : buttons) {
+            for (JButton button : row) {
+                button.setText(" ");
+            }
+        }
+        messageLabel.setText(boardLogic.currentPlayer());
+    }
+
+    public void onButtonClick(int x, int y) {
+        String message = switch (boardLogic.makeMove(x, y)) {
+            case MOVE -> boardLogic.currentPlayer();
+            case WINNER -> String.format("<-- %s wins -->", boardLogic.currentPlayer());
+            case TIE -> "<-- Game over -->";
+            case INVALID -> null;
+        };
+        if (message != null) {
+            messageLabel.setText(message);
+            buttons[x][y].setText(boardLogic.currentPlayer());
+        }
+    }
+
     private Dimension grisSize() {
         int size = BUTTON_SIZE * 3 + BUTTON_GAP * 3;
         return new Dimension(size, size);
     }
+
+
+    public static final int BUTTON_SIZE = 90, BUTTON_GAP = 3;
+    public static final Color
+            BOXCOLOR = new Color(0x283C4F),
+            BUTTONCOLOR = new Color(0x097DC9);
 
 }
